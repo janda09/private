c9fc9105f25ab8608478d0e62ae67b862848f4e5
janda09 <64169688+janda09@users.noreply.github.com>
2020-04-29 03:17:18 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -78,16 +78,18 @@ apt-get -y install nano iptables-persistent dnsutils screen whois ngrep unzip un
 echo "clear" >> .bashrc
 echo 'echo -e ""' >> .bashrc
 echo 'echo -e "   ██   ██  █████  ██████   █████  ██  "' >> .bashrc
-echo 'echo -e "   ██  ██  ██   ██ ██   ██ ██   ██ ██          "' >> .bashrc
-echo 'echo -e "   █████   ███████ ██   ██ ███████ ██           "' >> .bashrc
-echo 'echo -e "   ██  ██   ██   ██ ██   ██ ██   ██ ██   "' >> .bashrc
-echo 'echo -e "   ██   ██ ██   ██ ██████  ██   ██ ███████     "' >> .bashrc
-echo 'echo -e "███████  █████  ███    ███ ██ ██   ██    ██ "' >> .bashrc
-echo 'echo -e "██      ██   ██ ████  ████ ██ ██    ██  ██  "' >> .bashrc
+echo 'echo -e "   ██  ██  ██   ██ ██   ██ ██   ██ ██  "' >> .bashrc
+echo 'echo -e "   █████   ███████ ██   ██ ███████ ██  "' >> .bashrc
+echo 'echo -e "   ██  ██  ██   ██ ██   ██ ██   ██ ██  "' >> .bashrc
+echo 'echo -e "   ██   ██ ██   ██ ██████  ██   ██ ███████  "' >> .bashrc
+echo 'echo -e ""' >> .bashrc
+echo 'echo -e ""' >> .bashrc
+echo 'echo -e "███████  █████  ███    ███ ██ ██   ██    ██  "' >> .bashrc
+echo 'echo -e "██      ██   ██ ████  ████ ██ ██    ██  ██   "' >> .bashrc
 echo 'echo -e "█████   ███████ ██ ████ ██ ██ ██     ████   "'>> .bashrc
 echo 'echo -e "██      ██   ██ ██  ██  ██ ██ ██      ██    "' >> .bashrc
 echo 'echo -e "██      ██   ██ ██      ██ ██ ███████ ██    "' >> .bashrc
-echo 'echo -e "                                      The Big Kadall FamilyGroup™" | lolcat' >> .bashrc
+echo 'echo -e "                 The Big Kadall Family Group™" | lolcat' >> .bashrc
 echo 'echo -e "welcome to the server $HOSTNAME" | lolcat' >> .bashrc
 echo 'echo -e "Script mod by Rizwan Arif Firmansyah" | lolcat' >> .bashrc
 echo 'echo -e "Type menu to display a list of commands" | lolcat' >> .bashrc
@@ -97,10 +99,10 @@ echo 'echo -e ""' >> .bashrc
 cd
 rm /etc/nginx/sites-enabled/default
 rm /etc/nginx/sites-available/default
-wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/acillsadak/install/master/nginx.conf"
+wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/janda09/kadal/master/nginx.conf"
 mkdir -p /home/vps/public_html
 echo "<pre>Setup by JandaBaper</pre>" > /home/vps/public_html/index.html
-wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/acillsadak/install/master/vps.conf"
+wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/janda09/kadal/master/vps.conf"
 
 # install openvpn
 apt-get -y install openvpn easy-rsa openssl
@@ -143,27 +145,27 @@ chmod +x /etc/openvpn/ca.crt
 
 # server settings
 cd /etc/openvpn/
-wget -O /etc/openvpn/server.conf "https://raw.githubusercontent.com/acillsadank/install/master/server.conf"
+wget -O /etc/openvpn/server.conf "https://raw.githubusercontent.com/janda09/kadal/master/server.conf"
 systemctl start openvpn@server
 sysctl -w net.ipv4.ip_forward=1
 sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
 iptables -t nat -I POSTROUTING -s 192.168.100.0/24 -o eth0 -j MASQUERADE
 iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE
 iptables-save > /etc/iptables.up.rules
-wget -O /etc/network/if-up.d/iptables "https://raw.githubusercontent.com/acillsadank/install/master/iptables"
+wget -O /etc/network/if-up.d/iptables "https://raw.githubusercontent.com/janda09/kadal/master/iptables"
 chmod +x /etc/network/if-up.d/iptables
 sed -i 's|LimitNPROC|#LimitNPROC|g' /lib/systemd/system/openvpn@.service
 systemctl daemon-reload
 /etc/init.d/openvpn restart
 
 # openvpn config
-wget -O /etc/openvpn/client.ovpn "https://raw.githubusercontent.com/acillsadank/install/master/client.conf"
+wget -O /etc/openvpn/client.ovpn "https://raw.githubusercontent.com/janda09/kadal/master/client.conf"
 sed -i $MYIP2 /etc/openvpn/client.ovpn;
 echo '<ca>' >> /etc/openvpn/client.ovpn
 cat /etc/openvpn/ca.crt >> /etc/openvpn/client.ovpn
 echo '</ca>' >> /etc/openvpn/client.ovpn
 cp client.ovpn /home/vps/public_html/
-wget -O /etc/openvpn/openvpnssl.ovpn "https://raw.githubusercontent.com/acillsadank/install/master/openvpnssl.conf"
+wget -O /etc/openvpn/openvpnssl.ovpn "https://raw.githubusercontent.com/janda09/kadal/master/openvpnssl.conf"
 echo '<ca>' >> /etc/openvpn/openvpnssl.ovpn
 cat /etc/openvpn/ca.crt >> /etc/openvpn/openvpnssl.ovpn
 echo '</ca>' >> /etc/openvpn/openvpnssl.ovpn
@@ -171,9 +173,9 @@ cp openvpnssl.ovpn /home/vps/public_html/
 
 # install badvpn
 cd
-wget -O /usr/bin/badvpn-udpgw "https://raw.githubusercontent.com/acillsadank/install/master/badvpn-udpgw"
+wget -O /usr/bin/badvpn-udpgw "hhttps://raw.githubusercontent.com/janda09/kadal/master/badvpn-udpgw"
 if [ "$OS" == "x86_64" ]; then
-  wget -O /usr/bin/badvpn-udpgw "https://raw.githubusercontent.com/acillsadank/install/master/badvpn-udpgw64"
+  wget -O /usr/bin/badvpn-udpgw "https://raw.githubusercontent.com/janda09/kadal/master/badvpn-udpgw64"
 fi
 sed -i '$ i\screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7200' /etc/rc.local
 chmod +x /usr/bin/badvpn-udpgw
@@ -185,15 +187,15 @@ sed -i 's/Port 22/Port 22/g' /etc/ssh/sshd_config
 # install dropbear
 apt-get -y install dropbear
 sed -i 's/NO_START=1/NO_START=0/g' /etc/default/dropbear
-sed -i 's/DROPBEAR_PORT=22/DROPBEAR_PORT=143/g' /etc/default/dropbear
-sed -i 's/DROPBEAR_EXTRA_ARGS=/DROPBEAR_EXTRA_ARGS="-p 443 -p 990 "/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_PORT=22/DROPBEAR_PORT=443/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_EXTRA_ARGS=/DROPBEAR_EXTRA_ARGS="-p 443 -p 990"/g' /etc/default/dropbear
 echo "/bin/false" >> /etc/shells
 echo "/usr/sbin/nologin" >> /etc/shells
 /etc/init.d/dropbear restart
 
 # install squid
 apt-get -y install squid
-wget -O /etc/squid/squid.conf "https://raw.githubusercontent.com/acillsadank/install/master/squid3.conf"
+wget -O /etc/squid/squid.conf "https://raw.githubusercontent.com/janda09/kadal/master/squid3.conf"
 sed -i $MYIP2 /etc/squid/squid.conf;
 
 # install webmin
@@ -211,7 +213,7 @@ socket = r:TCP_NODELAY=1
 
 [dropbear]
 accept = 444
-connect = 127.0.0.1:143
+connect = 127.0.0.1:443
 
 [openvpn]
 accept = 442
@@ -228,7 +230,7 @@ cat key.pem cert.pem >> /etc/stunnel/stunnel.pem
 # configure stunnel
 sed -i 's/ENABLED=0/ENABLED=1/g' /etc/default/stunnel4
 cd /etc/stunnel/
-wget -O /etc/stunnel/ssl.conf "https://raw.githubusercontent.com/acillsadank/install/master/ssl.conf"
+wget -O /etc/stunnel/ssl.conf "https://raw.githubusercontent.com/janda09/kadal/master/ssl.conf"
 sed -i $MYIP2 /etc/stunnel/ssl.conf;
 cp ssl.conf /home/vps/public_html/
 cd
@@ -243,14 +245,14 @@ apt-get -y install fail2ban
 # install ddos deflate
 cd
 apt-get -y install dnsutils dsniff
-wget https://raw.githubusercontent.com/acillsadank/install/master/ddos-deflate-master.zip
+wget https://raw.githubusercontent.com/janda09/kadal/master/ddos-deflate-master.zip
 unzip ddos-deflate-master.zip
 cd ddos-deflate-master
 ./install.sh
 rm -rf /root/ddos-deflate-master.zip
 
 # banner /etc/bnr
-wget -O /etc/bnr "https://raw.githubusercontent.com/acillsadank/install/master/bnr"
+wget -O /etc/bnr "https://raw.githubusercontent.com/janda09/kadal/master/bnr"
 sed -i 's@#Banner@Banner@g' /etc/ssh/sshd_config
 sed -i 's@DROPBEAR_BANNER=""@DROPBEAR_BANNER="/etc/bnr"@g' /etc/default/dropbear
 
@@ -260,23 +262,23 @@ apt-get install -y libxml-parser-perl
 
 # download script
 cd /usr/bin
-wget -O menu "https://raw.githubusercontent.com/acillsadank/install/master/menu.sh"
-wget -O edit "https://raw.githubusercontent.com/acillsadak/install/master/edit-ports.sh"
-wget -O edit-dropbear "https://raw.githubusercontent.com/acillsadak/install/master/edit-dropbear.sh"
-wget -O edit-openssh "https://raw.githubusercontent.com/acillsadak/install/master/edit-openssh.sh"
-wget -O edit-openvpn "https://raw.githubusercontent.com/acillsadak/install/master/edit-openvpn.sh"
-wget -O edit-squid3 "https://raw.githubusercontent.com/acillsadak/install/master/edit-squid3.sh"
-wget -O edit-stunnel4 "https://raw.githubusercontent.com/acillsadak/install/master/edit-stunnel4.sh"
-wget -O show-ports "https://raw.githubusercontent.com/acillsadak/install/master/show-ports.sh"
-wget -O usernew "https://raw.githubusercontent.com/acillsadak/install/master/usernew.sh"
-wget -O trial "https://raw.githubusercontent.com/acillsadak/install/master/trial.sh"
-wget -O delete "https://raw.githubusercontent.com/acillsadak/install/master/delete.sh"
-wget -O check "https://raw.githubusercontent.com/acillsadak/install/master/user-login.sh"
-wget -O member "https://raw.githubusercontent.com/acillsadak/install/master/user-list.sh"
-wget -O restart "https://raw.githubusercontent.com/acillsadak/install/master/restart.sh"
-wget -O speedtest "https://raw.githubusercontent.com/acillsadak/install/master/speedtest_cli.py"
-wget -O info "https://raw.githubusercontent.com/acillsadak/install/master/info.sh"
-wget -O about "https://raw.githubusercontent.com/acillsadak/install/master/about.sh"
+wget -O menu "https://raw.githubusercontent.com/janda09/kadal/master/menu.sh"
+wget -O edit "https://raw.githubusercontent.com/janda09/kadal/master/edit-ports.sh"
+wget -O edit-dropbear "https://raw.githubusercontent.com/janda09/kadal/master/edit-dropbear.sh"
+wget -O edit-openssh "https://raw.githubusercontent.com/janda09/kadal/master/edit-openssh.sh"
+wget -O edit-openvpn "https://raw.githubusercontent.com/janda09/kadal/master/edit-openvpn.sh"
+wget -O edit-squid3 "https://raw.githubusercontent.com/janda09/kadal/master/edit-squid3.sh"
+wget -O edit-stunnel4 "https://raw.githubusercontent.com/janda09/kadal/master/edit-stunnel4.sh"
+wget -O show-ports "https://raw.githubusercontent.com/janda09/kadal/master/show-ports.sh"
+wget -O usernew "https://raw.githubusercontent.com/janda09/kadal/master/usernew.sh"
+wget -O trial "https://raw.githubusercontent.com/janda09/kadal/master/trial.sh"
+wget -O delete "https://raw.githubusercontent.com/janda09/kadal/master/delete.sh"
+wget -O check "https://raw.githubusercontent.com/janda09/kadal/master/user-login.sh"
+wget -O member "https://raw.githubusercontent.com/janda09/kadal/master/user-list.sh"
+wget -O restart "https://raw.githubusercontent.com/janda09/kadal/master/restart.sh"
+wget -O speedtest "https://raw.githubusercontent.com/janda09/kadal/master/speedtest_cli.py"
+wget -O info "https://raw.githubusercontent.com/janda09/kadal/master/info.sh"
+wget -O about "https://raw.githubusercontent.com/janda09/kadal/master/about.sh"
 
 chmod +x menu
 chmod +x edit
@@ -338,14 +340,14 @@ echo ""  | tee -a log-install.txt
 echo "Service"  | tee -a log-install.txt
 echo "-------"  | tee -a log-install.txt
 echo "OpenSSH  : 22"  | tee -a log-install.txt
-echo "Dropbear : 143, 443, 990"  | tee -a log-install.txt
+echo "Dropbear : 443, 990"  | tee -a log-install.txt
 echo "SSL      : 444" | tee -a log-install.txt
 echo "OpenVPNSSL : 442"  | tee -a log-install.txt
 echo "Squid3   : 80, 8080 (limit to IP SSH)"  | tee -a log-install.txt
 echo "SSL      : http://$MYIP:81/ssl.conf"  | tee -a log-install.txt
 echo "OpenVPNSSL: http://$MYIP:81/openvpnssl.ovpn"  | tee -a log-install.txt
 echo "OpenVPN  : TCP 1194 (client config : http://$MYIP:81/client.ovpn)"  | tee -a log-install.txt
-echo "badvpn   : badvpn-udpgw port 7500"  | tee -a log-install.txt
+echo "badvpn   : badvpn-udpgw port 7200"  | tee -a log-install.txt
 echo "nginx    : 81"  | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
 echo "Script"  | tee -a log-install.txt
