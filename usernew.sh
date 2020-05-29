61585b0543ffbe5b46f436b05baf0377bd1bc9b6
Janda Baper <64169688+janda09@users.noreply.github.com>
2020-05-29 12:49:31 +0700

    Update usernew.sh

--- usernew.sh
+++ usernew.sh
@@ -18,7 +18,7 @@ echo -e "Port OpenSSH: 22"  | lolcat
 echo -e "Port Dropbear: 143,110,109,456"  | lolcat
 echo -e "Port SSL: 443" | lolcat
 echo -e "Port Squid: 80,8080 (limit to IP SSH)" | lolcat
-echo -e "badvpn: badvpn-udpgw port 7500" | lolcat
+echo -e "badvpn: badvpn-udpgw port 7900" | lolcat
 echo -e "nginx: 81" | lolcat
 echo -e "=============================" | lolcat
 echo -e "Expiration: $exp" | lolcat
