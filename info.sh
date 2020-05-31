98fb18bce37d37f429513030c3c13bc4fea1749a
Janda Baper <64169688+janda09@users.noreply.github.com>
2020-06-01 02:15:09 +0700

    Update info.sh

--- info.sh
+++ info.sh
@@ -4,5 +4,8 @@
 clear
 neofetch
 echo -e ""
-echo -e "Ipang Nett Nott"
+echo -e "##############################################"
+echo -e "#              Janda Baper Group             #"
+echo -e "#               Ipang Nett Nott              #"
+echo -e "##############################################"
 echo -e ""
