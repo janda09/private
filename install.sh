c6b6960355fb901135b4f0b0e3ac8f4200fbca27
Janda Baper <64169688+janda09@users.noreply.github.com>
2020-06-01 02:00:08 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -98,13 +98,19 @@ wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/janda09/pr
 
 # install badvpn
 cd
-wget -O /usr/bin/badvpn-udpgw "https://raw.githubusercontent.com/janda09/private/master/badvpn-udpgw"
-if [ "$OS" == "x86_64" ]; then
-  wget -O /usr/bin/badvpn-udpgw "https://raw.githubusercontent.com/janda09/private/private/badvpn-udpgw64"
-fi
-sed -i '$ i\screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7900' /etc/rc.local
-chmod +x /usr/bin/badvpn-udpgw
-screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7900
+apt-get -y install cmake make gcc libc6-dev
+wget https://raw.githubusercontent.com/janda09/private/master/badvpn-1.999.128.tar.bz2
+tar xf badvpn-1.999.128.tar.bz2
+mkdir badvpn-build
+cd badvpn-build
+cmake badvpn-1.999.128 -DBUILD_NOTHING_BY_DEFAULT=1 -DBUILD_UDPGW=1
+make install
+echo 'badvpn-udpgw --listen-addr 127.0.0.1:7500 > /dev/nul &' >> /etc/rc.local
+badvpn-udpgw --listen-addr 127.0.0.1:7500 > /dev/nul &
+cd /usr/bin
+wget https://raw.githubusercontent.com/janda09/private/master/badvpn-udpgw
+chmod 755 badvpn-udpgw
+cd
 
 # setting port ssh
 sed -i 's/Port 22/Port 22/g' /etc/ssh/sshd_config
