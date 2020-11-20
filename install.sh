55d5c21e344c7851cd6bba201d1483b381b957f0
Janda Baper <64169688+janda09@users.noreply.github.com>
2020-11-20 19:02:24 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -232,13 +232,6 @@ chmod +x about
 # finishing
 cd
 chown -R www-data:www-data /home/vps/public_html
-/etc/init.d/nginx restart
-/etc/init.d/cron restart
-/etc/init.d/dropbear restart
-/etc/init.d/fail2ban restart
-/etc/init.d/webmin restart
-/etc/init.d/stunnel4 restart
-/etc/init.d/squid start
 rm -rf ~/.bash_history && history -c
 echo "unset HISTFILE" >> /etc/profile
 
