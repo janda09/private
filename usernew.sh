2b2e8d04e0fbb8e4f51b9f7d9b659f739aa4cf08
Janda Baper <64169688+janda09@users.noreply.github.com>
2020-11-27 02:04:58 +0700

    Update usernew.sh

--- usernew.sh
+++ usernew.sh
@@ -15,7 +15,7 @@ echo -e "Host: $IP" | lolcat
 echo -e "Username: $Login " | lolcat
 echo -e "Password: $Pass" | lolcat
 echo -e "Port OpenSSH: 22"  | lolcat
-echo -e "Port Dropbear: 143, 80, 443,456"  | lolcat
+echo -e "Port Dropbear: 143, 80, 110, 456"  | lolcat
 echo -e "Port SSL: 443" | lolcat
 echo -e "Port Squid: 3128,8080 (limit to IP SSH)" | lolcat
 echo -e "badvpn: badvpn-udpgw port 7300" | lolcat
